75080b689b33880db229724c1818b32987f0b356
Rich Garcia <rchgrca@yahoo.com>
2017-07-17 10:30:41 -0700

    removes console.log

--- src/views/podcast-list-item.js
+++ src/views/podcast-list-item.js
@@ -6,7 +6,7 @@ function PodcastListItem(props) {
   const { title, slug, image = [] } = props;
   const imgSrc = image[0] ? image[0].url : props["itunes:image"][0].$.href;
   const imgAlt = image[0] ? image[0].title : props.slug;
-  console.log(imgAlt)
+
   return (
     <li className={styles.podcastListItem} >
       <Link className={styles.itemLink} to={`/${slug}`} data-title={title}>
