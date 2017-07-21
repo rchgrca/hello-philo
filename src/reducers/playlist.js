d557582fd69f3ac72065631da4ef892664059b40
Rich Garcia <rchgrca@yahoo.com>
2017-07-21 13:36:35 -0700

    initial version

---
+++ src/reducers/playlist.js
@@ -0,0 +1,13 @@
+import { ADD_TO_PLAYLIST, DELETE_FROM_PLAYLIST } from '../actions';
+import { createReducer } from '../utils';
+
+const addEpisode = (state, { payload }) => ({ ...state, ...payload });
+
+const deleteEpisode = (state, { payload }) => ({ ...state, ...payload });
+
+const handlers = {
+  [ADD_TO_PLAYLIST]: addEpisode,
+  [DELETE_FROM_PLAYLIST]: deleteEpisode
+};
+
+export default createReducer({}, handlers);
