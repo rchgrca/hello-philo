314e38a486339d7f6acd08a45e911e73a5984cba
Rich Garcia <rchgrca@yahoo.com>
2017-08-05 13:33:29 -0700

    corrects linting errors

--- src/reducers/playlist.js
+++ src/reducers/playlist.js
@@ -1,26 +1,26 @@
 import { ADD_TO_PLAYLIST, DELETE_FROM_PLAYLIST } from '../actions';
 import { createReducer } from '../utils';
 
-const addEpisode = (state, { payload }) => ({ ...state, [payload.title]:payload });
+const addEpisode = (state, { payload }) => ({ ...state, [payload.title]: payload });
 
 const deleteEpisode = (state, { payload }) => {
-    // create an array with the state's key/value pairs returned
-    const tempStateEntries = Object.entries({...state});
-    // remove episode by filtering array, then convert it back to an object
-    const filteredtempState = tempStateEntries.filter((episode) => {
-        return episode[0] !== payload.title;
-    }).reduce((obj, episode) => {
-        obj[episode[0]] = episode[1]
-        return obj
-    },{});
-    return {
-        ...filteredtempState
-    }
-}
+  // create an array with the state's key/value pairs returned
+  const tempStateEntries = Object.entries({ ...state });
+  // remove episode by filtering array, then convert it back to an object
+  const filteredtempState = tempStateEntries
+    .filter(episode => episode[0] !== payload.title)
+    .reduce((obj, episode) => {
+      obj[episode[0]] = episode[1];
+      return obj;
+    }, {});
+  return {
+    ...filteredtempState,
+  };
+};
 
 const handlers = {
   [ADD_TO_PLAYLIST]: addEpisode,
-  [DELETE_FROM_PLAYLIST]: deleteEpisode
+  [DELETE_FROM_PLAYLIST]: deleteEpisode,
 };
 
 export default createReducer({}, handlers);
