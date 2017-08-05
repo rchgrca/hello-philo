5532b3674a177bdb364b8b7708aa69d84d1ff254
Rich Garcia <rchgrca@yahoo.com>
2017-08-05 09:03:13 -0700

    adds comments

--- src/reducers/playlist.js
+++ src/reducers/playlist.js
@@ -4,7 +4,9 @@ import { createReducer } from '../utils';
 const addEpisode = (state, { payload }) => ({ ...state, [payload.title]:payload });
 
 const deleteEpisode = (state, { payload }) => {
+    // create an array with the state's key/value pairs returned
     const tempStateEntries = Object.entries({...state});
+    // remove episode by filtering array, then convert it back to an object
     const filteredtempState = tempStateEntries.filter((episode) => {
         return episode[0] !== payload.title;
     }).reduce((obj, episode) => {
