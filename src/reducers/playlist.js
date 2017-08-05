3207c4eeb79854686db6b5b06736ed55b969c8e9
Rich Garcia <rchgrca@yahoo.com>
2017-08-04 17:58:46 -0700

    support for delete playist episode

--- src/reducers/playlist.js
+++ src/reducers/playlist.js
@@ -3,7 +3,21 @@ import { createReducer } from '../utils';
 
 const addEpisode = (state, { payload }) => ({ ...state, [payload.title]:payload });
 
-const deleteEpisode = (state, { payload }) => ({ ...state, ...payload });
+const deleteEpisode = (state, { payload }) => {
+    const tempState = { ...state };
+    const tempStateEntries = Object.entries(tempState);
+    const filteredtempStateArray = tempStateEntries.filter((episode) => {
+        return episode[0] !== payload.title;
+    });
+    const filteredtempStateObject = {}
+    const filteredtempStateObjectCreator = filteredtempStateArray.map(episode => {
+        filteredtempStateObject[episode[0]] = episode[1];
+        return filteredtempStateObject;
+    });
+    return {
+        ...filteredtempStateObject
+    }
+}
 
 const handlers = {
   [ADD_TO_PLAYLIST]: addEpisode,
