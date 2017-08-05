938a6ce861a3663fa6bf7d56cbcd10d5e0fe856d
Rich Garcia <rchgrca@yahoo.com>
2017-08-05 14:36:43 -0700

    fixes linting errors

--- src/views/podcast-episode-list.js
+++ src/views/podcast-episode-list.js
@@ -4,8 +4,21 @@ import { bindActionCreators } from 'redux';
 import * as podcastActions from '../action-creators/podcasts';
 import styles from '../styles/podcast-episode-list';
 
+function addToPlaylist(props, ep) {
+  props.addPodcastEpisodeToPlaylist(ep);
+}
+
+function getAddToPlaylistData() {
+  return {
+    fonticon: 'fa-plus-square',
+    title: 'Add to playlist',
+    handler: addToPlaylist,
+  };
+}
+
 function PodcastEpisodeList(props) {
-  const { title: [podcastTitle = ''] = [], item: episodes = [], routeParams } = props;
+  const { title: [podcastTitle = ''] = [], item: episodes = [],
+    routeParams } = props;
 
   const formattedEps = episodes.map(({ enclosure, title: [title] }) => ({
     podcastTitle,
@@ -14,7 +27,7 @@ function PodcastEpisodeList(props) {
     podcastId: routeParams.podcastId,
   }));
 
-  const playlistData = (true) ? getAddToPlaylistData() : getDeleteFromPlaylistData();
+  const playlistData = getAddToPlaylistData();
 
   return (
     <div className={styles.episodeListContainer}>
@@ -23,7 +36,9 @@ function PodcastEpisodeList(props) {
         {
           formattedEps.map((ep, i) => (
             <li key={`${i}-${ep.title}`} className={styles.episodeListItem} title={ep.title}>
-              <div className={styles.episodeButton}><i className={`fa ${playlistData.fonticon}`} aria-hidden="true" title={playlistData.title} onClick={() => playlistData.handler(props, ep)}></i></div>
+              <div className={styles.episodeButton}>
+                <i className={`fa ${playlistData.fonticon}`} title={playlistData.title} onClick={() => playlistData.handler(props, ep)}></i>
+              </div>
               <div className={styles.episodeLink} onClick={() => props.loadPodcastEpisode(ep)}>
                 {ep.title}
               </div>
@@ -33,30 +48,6 @@ function PodcastEpisodeList(props) {
       </ul>
     </div>
   );
-}
-
-function addToPlaylist(props, ep){
-    props.addPodcastEpisodeToPlaylist(ep)
-}
-
-function deleteFromPlaylist(props, ep){
-    props.deletePodcastEpisodeFromPlaylist(ep)
-}
-
-function getAddToPlaylistData(){
-    return {
-        fonticon: "fa-plus-square",
-        title: "Add to playlist",
-        handler: addToPlaylist
-    }
-}
-
-function getDeleteFromPlaylistData(){
-    return {
-        fonticon: "fa-minus-square",
-        title: "Delete from playlist",
-        handler: deleteFromPlaylist
-    }
 }
 
 PodcastEpisodeList.propTypes = {
