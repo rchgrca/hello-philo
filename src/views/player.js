e8e86f0d5f9024b567cc6b3b0bcd93b4396235b3
Rich Garcia <rchgrca@yahoo.com>
2017-08-08 18:51:18 -0700

    fixes lint errors

--- src/views/player.js
+++ src/views/player.js
@@ -6,20 +6,18 @@ import * as playerActions from '../action-creators/player';
 import styles from '../styles/player';
 
 function Player(props) {
-  const { player: {
-    title, podcastTitle, src
-  }, playlist } = props;
+  const { player: { title, podcastTitle, src }, playlist, loadPodcastEpisode } = props;
 
   const aPlaylistTitles = Object.keys(playlist);
   const getNextPlaylistTitle = () => {
     const nextIndex = aPlaylistTitles.findIndex((element) => element === title) + 1;
     const index = nextIndex === aPlaylistTitles.length ? 0 : nextIndex;
     return playlist[aPlaylistTitles[index]];
-  }
+  };
 
   const handleEpisodeEnd = () => {
-    props.loadPodcastEpisode(getNextPlaylistTitle());
-  }
+    loadPodcastEpisode(getNextPlaylistTitle());
+  };
 
   const audioEl = src ? <audio src={src} controls autoPlay onEnded={handleEpisodeEnd} /> : null;
 
@@ -37,6 +35,9 @@ Player.propTypes = {
   title: PropTypes.string,
   podcastTitle: PropTypes.string,
   src: PropTypes.string,
+  player: PropTypes.object,
+  playlist: PropTypes.object,
+  loadPodcastEpisode: PropTypes.func,
 };
 
 const mapStateToProps = (state) => state;
