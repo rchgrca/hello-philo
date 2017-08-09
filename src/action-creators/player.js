93fbd7d619db57ca2dfb38dbdd535c5a128e7c55
Rich Garcia <rchgrca@yahoo.com>
2017-08-09 15:57:40 -0700

    fixes bug on onEnded

--- src/action-creators/player.js
+++ src/action-creators/player.js
@@ -1,6 +1,6 @@
 import { LOAD_PODCAST_EPISODE } from '../actions';
 
-export default function loadPodcastEpisode(episode) {
+export function loadPodcastEpisode(episode) {
   return {
     type: LOAD_PODCAST_EPISODE,
     payload: episode,
