b213f2fd1d45495b3f863dcb46ae2389ee269af5
Rich Garcia <rchgrca@yahoo.com>
2017-08-08 18:25:00 -0700

    removes Sidebar, adds Logo to top of app and CSS

--- src/views/app.js
+++ src/views/app.js
@@ -1,14 +1,16 @@
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 import styles from '../styles/app';
 import Sidebar from './sidebar';
 import Player from './player';
+import { LogoIcon } from './icons';
 
 function App(props) {
   return (
     <div className={styles.layout}>
       <div className={styles.topLayout}>
-        <Sidebar />
         <div className={styles.main}>
+          <Link to="/" className={styles.logo}><LogoIcon classNames={styles.icon} /></Link>
           {props.children}
         </div>
       </div>
