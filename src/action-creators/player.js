867aee6dfea9ec4109c4da9a85dfcfbe41bde8fb
Rich Garcia <rchgrca@yahoo.com>
2017-08-08 18:28:30 -0700

    fixes lint error

--- src/action-creators/player.js
+++ src/action-creators/player.js
@@ -1,6 +1,6 @@
 import { LOAD_PODCAST_EPISODE } from '../actions';
 
-export function loadPodcastEpisode(episode) {
+export default function loadPodcastEpisode(episode) {
   return {
     type: LOAD_PODCAST_EPISODE,
     payload: episode,
