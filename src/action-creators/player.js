0c311550692ec2ad4c94c1315f9da5543222519a
Rich Garcia <rchgrca@yahoo.com>
2017-08-10 16:39:08 -0700

    remove player action-creator player.js since its functions are identical to playlist.js, update playerActions

--- src/action-creators/player.js
+++
@@ -1,8 +0,0 @@
-import { LOAD_PODCAST_EPISODE } from '../actions';
-
-export function loadPodcastEpisode(episode) {
-  return {
-    type: LOAD_PODCAST_EPISODE,
-    payload: episode,
-  };
-}
--- src/views/player.js
+++ src/views/player.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router';
-import * as playerActions from '../action-creators/player';
+import * as playerActions from '../action-creators/playlist';
 import { isEmpty } from '../utils';
 import styles from '../styles/player';
 
